a0247f5ede08cc47336cc0c5122213b669647199
MagnunAVF <magnunavf@gmail.com>
2025-08-03 00:26:16 -0300

    ci debug

--- scripts/create_ecr.sh
+++ scripts/create_ecr.sh
@@ -3,8 +3,10 @@
 set +e
 
 REPO_EXISTS=$(aws ecr describe-repositories --repository-names "${REPOSITORY_NAME}" 2>&1)
+echo $REPO_EXISTS
 if [[ $REPO_EXISTS == *"RepositoryNotFoundException"* ]]; then
     echo "Repository ${REPOSITORY_NAME} not found. Creating..."
+
     if aws ecr create-repository --repository-name ${REPOSITORY_NAME}; then
         echo "Successfully created repository ${REPOSITORY_NAME}"
     else
